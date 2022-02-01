7b699541c0e1f5e451b15aebc76f8c2475dbf628
loesel19 <aloesel2@gmail.com>
2022-02-01 15:14:59 -0500

    commit

---
+++ HW1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "prog1.h"
+int main()
+{
+
+}
---
+++ prog1.h
@@ -0,0 +1,7 @@
+int num_stds;           // number of students
+struct StudentGrade {
+   int id;              // student id
+   char* name;          // student name;
+   int math116;
+   int cs116;
+};
