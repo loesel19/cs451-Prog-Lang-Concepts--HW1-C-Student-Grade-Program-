875103b4b08009ab8741e24e893195c44f6b37ea
loesel19 <aloesel2@gmail.com>
2022-02-07 15:04:28 -0500

    report now prints

--- HW1.c
+++ HW1.c
@@ -18,7 +18,10 @@ scanf("%s",fileName);
 int intfExists = exists(fileName);
 if (intfExists == 1)
 {
+    //read file
     intStatus = readFile(fileName);
+    //print out report
+    printReport();
 }else{
     printf("Could not find file: \n");
     printf(fileName);
@@ -44,35 +47,120 @@ int readFile(char *fileName){
         printf("number of students must be > 0.\n");
         return 1;
     }
-    
+    /*
+    I have come across two options for continuing here.
+    1: I could send a struct holding the current student being read from the file to a
+       method to print it out, and inside this readFile method I would maintain arrays
+       of the students scores to later calculate the averages with.
+    2: create an array of students that is pointed to by a global scope pointer so that
+       student data can be accessed from any method in this file.
+    1 is probably an easier roundabout way of doing this program, but 2 seems like a less
+    roundabout method and more solid approach to the problem. Plus doing it this way I
+    will get to practice with pointers more, so I will do 2.
+    */
     int i = 0;//index for array and counter for while loop
     printf("num_Stds: %d\n", num_stds);
-    //allocate space for our array of studentgrades
-    
+    //allocate proper amount of space for our students array
+    students = malloc(num_stds * (sizeof(struct StudentGrade)));
     //loop through all the student info, since reading each student into 
     while (i < num_stds)
     {
-        //create our student grade struct
-        struct StudentGrade temp;
+        struct StudentGrade *ptr;//pointer to used to point to our StudentGrade
+        struct StudentGrade temp;//tempory studentGrade
         //allocate memory for name, say 8bits for 30 chars
         //temp.name = malloc(240);
         //first line is going to be name
         temp.name = strdup(fgets(buff, 255, (FILE*)file));
+        //we want to remove new line character from the name
+        strtok(temp.name, "\n");
         //next line is id
         temp.id = atoi(fgets(buff, 255, (FILE*)file));
         //next line is math then cs score
         fscanf(file, "%d", &temp.math116);
         fscanf(file, "%d", &temp.cs116);
         fgets(buff, 255, (FILE*)file); //we read the next line so that the file is read correctly
-    printf("name, %s : id, %i : math, %d : cs, %d\n", temp.name, temp.id, temp.math116, temp.cs116);
-        i++;
+    //printf("name, %s : id, %i : math, %d : cs, %d\n", temp.name, temp.id, temp.math116, temp.cs116);
+       
         //we need to point to this structure with our array of students
-        students + i = temp;
+        ptr = &temp;
+        *(students + i) = *ptr;
+        i++;
+       
+
     }
-    
     return 0;
       
 }//end readFile
+
+void printReport(){
+//first print out the headers
+printf(" Student Name      Student ID      Math 116   CS 116\n");
+printf("--------------     ----------      --------   ------\n");
+//now we loop through the array of students that we created and print out their data
+for (size_t i = 0; i < num_stds; i++){
+    struct StudentGrade currStudent = *(students+ i);
+    //print name and id
+    printf("%*.*s     %*i",14,14, currStudent.name, 10, currStudent.id);
+    //print math score with grade
+    char grade = calculateGrade(currStudent.math116); 
+    printf("      %*d(%c)", 5, currStudent.math116, grade);
+    //get cs grade
+    grade = calculateGrade(currStudent.cs116);
+    printf("   %*d(%c)\n", 3, currStudent.cs116, grade);
+    
+}
+//print a break before we print out the class averages
+printf("====================================================\n");
+double mathAverage = getMathAverage();
+double csAverage = getCSAverage();
+//now print class averages
+printf("Math 116 Average: %*.*f\n",3,2, mathAverage);
+printf("CS 116 Average: %*.*f\n",3,2, csAverage);
+printf("*** END OF REPORT ***");
+
+
+}//end printReport
+char calculateGrade(int score){
+    if (score < 60){
+        return 'F';
+    }else if (score < 70){
+        return 'D';
+    }else if(score < 80){
+        return 'C';
+    }else if(score < 90){
+        return 'B';
+    }else{
+        return 'A';
+    }
+    
+    
+}//end calculateGrade
+double getMathAverage(){
+int total = 0; //the total so far
+//loop through students and add each score to total
+for(size_t i = 0; i < num_stds; i++){
+    struct StudentGrade currStudent= *(students + i); //current student
+    total = total + currStudent.math116;
+}
+//now we have to convert total to a double
+total = total * 100;
+double dblTotal = (double)total / (100.00 * num_stds);
+//now we return the total/num of students as a double
+return dblTotal;
+}
+double getCSAverage(){
+int total = 0; //the total so far
+//loop through students and add each score to total
+for(size_t i = 0; i < num_stds; i++){
+    struct StudentGrade currStudent= *(students + i); //current student
+    total = total + currStudent.cs116;
+}
+//now we convert total to a double
+total = total * 100;
+double dblTotal = (double)total / (100.00 * num_stds);
+//now we return the total/num of students as a double
+return dblTotal;
+}
 
 int exists(char *fileName){
     FILE *file;
--- prog1.h
+++ prog1.h
@@ -3,6 +3,10 @@ int num_stds;           // number of students
 //function declarations
 int readFile(char *fileName);//read data from file
 int exists(char *fileName);//check if file exists
+char calculateGrade(int score);//calculate grade
+void printReport();//prints out the report at the end
+double getMathAverage();//gets the average of all students scores
+double getCSAverage();//gets the average of all students scores
 
 struct StudentGrade {
    int id;              // student id
