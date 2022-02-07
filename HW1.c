e6cb50b32091a4b155798692ede5ac606f23cacb
loesel19 <aloesel2@gmail.com>
2022-02-07 00:34:44 -0500

    got the readFile to work,, kinda.

--- HW1.c
+++ HW1.c
@@ -3,21 +3,26 @@
 #include <string.h>
 #include "prog1.h"
 
-int readFile(char *fileName);
-int exists(char *fileName);
-int main()
-{
+
+
+struct StudentGrade *students;// array of students
+
+int main(){
+
     char *fileName = malloc(100); //file name from input
     int intStatus; //status code for main function
+    
 printf("Enter input file name: ");
 //read input into a string and call the readFile method with str args
-scanf(fileName);
+scanf("%s",fileName);
 int intfExists = exists(fileName);
 if (intfExists == 1)
 {
     intStatus = readFile(fileName);
 }else{
-    printf("Could not find that file.");
+    printf("Could not find file: \n");
+    printf(fileName);
+    //printf("77");
     return 1;
 }
 return intStatus;
@@ -29,44 +34,44 @@ return intStatus;
 int readFile(char *fileName){
     //local vars
     FILE *file = fopen(fileName, "r"); //file we are reading
-    if (file == NULL)
+    char buff[255];//file buffer
+    //get the number of students from the first line
+    fgets(buff, 255, (FILE*)file);
+    //set num_stds from prog1.h
+    num_stds = atoi(buff);
+    if (num_stds <= 0)
     {
-        perror("Unable to open file");
+        printf("number of students must be > 0.\n");
         return 1;
     }
-    char chunk[128];
-    size_t len = sizeof(chunk);
-    char *line = malloc(len);
-    if (line == NULL)
+    
+    int i = 0;//index for array and counter for while loop
+    printf("num_Stds: %d\n", num_stds);
+    //allocate space for our array of studentgrades
+    
+    //loop through all the student info, since reading each student into 
+    while (i < num_stds)
     {
-        perror("Unable to allocate memory for the line buffer.");
-        return 1;
-    }
-    line[0] = "\0"; //empty the string
-    while (fgets(chunk, sizeof(chunk), file) != NULL)
-    {
-        size_t len_used = strlen(line);
-        size_t chunk_used = strlen(chunk);
-
-        if (len - len_used < chunk_used)
-        {
-            len *= 2;
-            if(line = realloc(line, len) == NULL){
-                perror("Unable to reallocate memory for the line buffer.");
-                free(line);
-                return 1;
-            }
-        }
-
-        strncpy(line + len_used, chunk, len - len_used);
-        len_used += chunk_used;
-        fputs(line, stdout);
-        
+        //create our student grade struct
+        struct StudentGrade temp;
+        //allocate memory for name, say 8bits for 30 chars
+        //temp.name = malloc(240);
+        //first line is going to be name
+        temp.name = strdup(fgets(buff, 255, (FILE*)file));
+        //next line is id
+        temp.id = atoi(fgets(buff, 255, (FILE*)file));
+        //next line is math then cs score
+        fscanf(file, "%d", &temp.math116);
+        fscanf(file, "%d", &temp.cs116);
+        fgets(buff, 255, (FILE*)file); //we read the next line so that the file is read correctly
+    printf("name, %s : id, %i : math, %d : cs, %d\n", temp.name, temp.id, temp.math116, temp.cs116);
+        i++;
+        //we need to point to this structure with our array of students
+        students + i = temp;
     }
     
-    
-    
-    
+    return 0;
+      
 }//end readFile
 
 int exists(char *fileName){
--- prog1.h
+++ prog1.h
@@ -1,4 +1,9 @@
 int num_stds;           // number of students
+
+//function declarations
+int readFile(char *fileName);//read data from file
+int exists(char *fileName);//check if file exists
+
 struct StudentGrade {
    int id;              // student id
    char* name;          // student name;
